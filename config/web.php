687124daa4a0fd815753c64a9b77479473896201
Dmitry Zozulya <agent3030@gmail.com>
2017-02-11 23:29:35 +0200

    added base migrations setup

--- config/web.php
+++ config/web.php
@@ -5,21 +5,43 @@
  * Date: 09.02.17
  * Time: 13:14
   * */
-
+$db_local = require(__DIR__ . '/db-local.php');
+$db_server = require(__DIR__ . '/db-server.php');
 
 return [
-    'id' => 'yii2-base-template',
+    'id' => 'basic',
+    'name' => 'yii2_base_template',
+    'layout' => "@app/modules/main/views/layouts/main.php",
+    //'layout' => "@app/modules/main/views/layouts/main.twig",
     'basePath' => realpath (__DIR__ . '/../'),
+    'modules' => [
+        'user' => [
+            'class' => 'app\modules\user\Module',
+        ],
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+        'main' => [
+            'class' => 'app\modules\main\Module',
+        ]
+    ],
     'components' => [
         'request' => [
-            'cookieValidationKey' => 'your secret key here',
+            'cookieValidationKey' => env('APP_COOKIE_VALIDATION_KEY'),
+        ],
+        'user' => [
+            'class'=>'yii\web\User',
+            'identityClass' => 'app\models\User',
+            'loginUrl'=>['/user/sign-in/login'],
+            'enableAutoLogin' => true,
+            'as afterLogin' => 'app\behaviors\LoginTimestampBehavior'
         ],
         'errorHandler' => [
-            'errorAction' => '/site/error',
+            'errorAction' => '/main/site/error',
         ],
-        'db' => require(__DIR__ . '/db-local.php'),
-        //uncoment when use on hosting
-        //'db' => require(__DIR__ . '/db-server.php'),
+        'db' => $db_local,
+        //uncomment when use on hosting
+        //'db' => $db_server,
 
     ]
 ];
